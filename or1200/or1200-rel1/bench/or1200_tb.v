1a582fe5daba7ddc36572390b8b22654bc6bdf73
OpenRISC user <openrisc@openrisc-VirtualBox.(none)>
2017-02-04 10:39:10 +0100

    finished tb initial verison

--- or1200/or1200-rel1/bench/or1200_tb.v
+++ or1200/or1200-rel1/bench/or1200_tb.v
@@ -41,6 +41,48 @@ module or1200_tb();
    .iwb_cab_o(),
    `endif
 
-  );
+   // data wishbond bus interface
+   .dwb_clk_i(clk_i),
+   .dwb_rst_i(rst),
+   .dwb_dat_i(32'b0),
+   .dwb_ack_i(1'b0),
+   .dwb_err_i(1'b0),
+   .dwb_rty_i(1'b0),
+   .dwb_cyc_o(),
+   .dwb_adr_o(),
+   .dwb_dat_o(),
+   .dwb_stb_o(),
+   .dwb_we_o(),
+   .dwb_sel_o(),
+   `ifdef OR1200_WB_CAB
+   .dwb_cab_o(),
+   `endif
+
+   // external debug interface
+   .dbg_stall_i(1'b0),
+   .dbg_ewt_i(1'b0),
+   .dbg_lss_o(1'b0),
+   .dbg_is_o(),
+   .dbg_wp_o(),
+   .dbg_bp_o(),
+   .dbg_stb_i(1'b0),
+   .dbg_we_i(1'b0),
+   .dbg_adr_i(0),
+   .dbg_dat_i(0),
+   .dbg_dat_o(),
+   .dbg_ack_o(),
+
+   // Power management unit interface
+   .pm_cpustall_i(0),
+   .pm_clksd_o(),
+   .pm_dc_gate_o(),
+   .pm_ic_gate_o(),
+   .pm_dmmu_gate_o(),
+   .pm_immu_gate_o(),
+   .pm_tt_gate_o(),
+   .pm_cpu_gate_o(),
+   .pm_wakeup_o(),
+   .pm_lvolt_o()
+);
 
 endmodule
